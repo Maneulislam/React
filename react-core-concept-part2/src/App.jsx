b3ab1e1a1af112092fda43493f60f57a1c967fad
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2025-11-26 11:38:49 +0600

    Basic react practice

--- react-core-concept-part2/src/App.jsx
+++ react-core-concept-part2/src/App.jsx
@@ -3,10 +3,16 @@ import './App.css'
 
 function App() {
 
+  const handleClick = (num) => {
+    alert(num + 5)
+  }
+
   return (
     <>
 
       <h1>Vite + React</h1>
+
+      <button onClick={() => handleClick(10)}>Click Add 5</button>
 
     </>
   )
